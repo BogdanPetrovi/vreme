55f332338e61a6f8ac78bc763aa6371d255b713c
Bogdan Petrovic <160347824+BogdanPetrovi@users.noreply.github.com>
2024-12-08 00:21:34 +0100

    Update api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: 'https://vreme-one.vercel.app',
+  baseURL: 'https://vreme-3bk0.onrender.com',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
